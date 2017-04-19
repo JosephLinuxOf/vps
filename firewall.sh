32494c1420d7ab475c8588e97698ade10994611a
Joseph Linux <eipluslinuxbr@gmail.com>
2017-04-18 20:47:14 -0400

    Update firewall.sh

--- firewall.sh
+++ firewall.sh
@@ -6,6 +6,7 @@ iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
 iptables -t filter -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -m limit --limit 100/minute --limit-burst 200 -j ACCEPT
 iptables -A OUTPUT -p tcp -d $ip --dport 443 -m state --state NEW -j ACCEPT
 iptables -A OUTPUT -p tcp -d $ip --dport 80 -m state --state NEW -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 53 -m state --state NEW -j ACCEPT
@@ -31,3 +32,10 @@ iptables -A FORWARD -p tcp --dport 8799 -j ACCEPT
 iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
 iptables -A INPUT -p tcp --dport 10000 -j ACCEPT
 iptables -A OUTPUT -p tcp --dport 10000 -j ACCEPT
+
+# Bloqueando elementos não costantes nas regras
+
+iptables -A INPUT -p tcp --syn -j DROP
+
+# A regra abaixo limita o numero de conexes SSH vindas do mesmo IP
+# iptables -A INPUT -p tcp --syn --dport 22 -m connlimit --connlimit-above 1 -j REJECT
