fc73926c68f2d66836fcedc278cc535c802e14c6
Joseph Linux <eipluslinuxbr@gmail.com>
2017-04-18 20:38:24 -0400

    Create firewall.sh

---
+++ firewall.sh
@@ -0,0 +1,33 @@
+ip=$(wget -qO - icanhazip.com)
+iptables -P INPUT DROP
+iptables -P OUTPUT DROP
+iptables -P FORWARD DROP
+iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A OUTPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -A FORWARD -m state --state ESTABLISHED,RELATED -j ACCEPT
+iptables -t filter -A INPUT -m state --state RELATED,ESTABLISHED -j ACCEPT
+iptables -A OUTPUT -p tcp -d $ip --dport 443 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p tcp -d $ip --dport 80 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p udp --dport 53 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 67 -m state --state NEW -j ACCEPT
+iptables -A OUTPUT -p udp --dport 67 -m state --state NEW -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+iptables -A INPUT -p tcp --dport 443 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 22 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 443 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+iptables -A INPUT -p tcp --dport 3128 -j ACCEPT
+iptables -A INPUT -p tcp --dport 8799 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 8080 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 80 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 3128 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 8799 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 8080 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 80 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 3128 -j ACCEPT
+iptables -A FORWARD -p tcp --dport 8799 -j ACCEPT
+iptables -A INPUT -p icmp --icmp-type echo-request -j DROP
+iptables -A INPUT -p tcp --dport 10000 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 10000 -j ACCEPT
